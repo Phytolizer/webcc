7703d55b7bb33b7235ad9c6de5de8e34a5b6d77c
Kyle Coffey <me@phytolizer.dev>
2023-07-17 02:01:18 -0500

    use Fastify for a real API

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,92 @@
 import { generateProgram } from "./backend.mjs";
 import { lex } from "./lexer.mjs";
 import { parse } from "./parser.mjs";
+import Fastify from "fastify";
 
-document.body.addEventListener("click", (event) => {
-  if (event.target.id === "compile") {
-    const source = document.getElementById("source").value;
-    const tokens = lex(source);
-    document.getElementById("lexed").value = JSON.stringify(tokens, null, 2);
-    const ast = parse(tokens);
-    document.getElementById("parsed").value = JSON.stringify(ast, null, 2);
-    document.getElementById("assembly").value = generateProgram(ast);
+const fastify = Fastify({ logger: true });
+
+const schema = {
+  body: {
+    type: "object",
+    properties: {
+      type: { enum: ["lex", "parse", "compile"] },
+    },
+    required: ["type"],
+    anyOf: [
+      {
+        properties: {
+          type: { enum: ["lex", "parse"] },
+          source: { type: "string" },
+        },
+        required: ["source"],
+      },
+    ],
+    oneOf: [
+      {
+        properties: {
+          type: { const: "compile" },
+          source: { type: "string" },
+        },
+        required: ["source"],
+      },
+      {
+        properties: {
+          type: { const: "compile" },
+          tokens: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                type: { type: "string" },
+                value: { type: "string" },
+              },
+              required: ["type", "value"],
+            },
+          },
+        },
+        required: ["tokens"],
+      },
+      {
+        properties: {
+          type: { const: "compile" },
+          ast: {
+            type: "object",
+            properties: {
+              type: { type: "string" },
+            },
+            required: ["type"],
+          },
+        },
+        required: ["ast"],
+      },
+    ],
+  },
+};
+
+fastify.post("/", { schema: { body: schema } }, async (request, reply) => {
+  const { type } = request.body;
+  if (type === "lex") {
+    const { source } = request.body;
+    reply.send(lex(source));
+  } else if (type === "parse") {
+    const { source } = request.body;
+    reply.send(parse(source));
+  } else if (type === "compile") {
+    const { source, tokens, ast } = request.body;
+    if (source) {
+      reply.send({ asm: generateProgram(parse(lex(source))) });
+    } else if (tokens) {
+      reply.send({ asm: generateProgram(parse(tokens)) });
+    } else if (ast) {
+      reply.send({ asm: generateProgram(ast) });
+    }
   }
+});
+
+fastify.listen({ port: 8080 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
 });
