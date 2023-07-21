85873ae08da1c00a73c561f3c3296038e2fc47bb
Kyle Coffey <me@phytolizer.dev>
2023-07-21 15:32:29 -0500

    style dev site a bit, clean up webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import * as path from 'path'
+import { Configuration } from 'webpack'
+
+export default (env: any, argv: { mode: string }): Configuration => {
+  let result: Configuration = {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node-modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  }
+
+  if (argv.mode === 'development') {
+    result.devtool = 'inline-source-map'
+  }
+
+  return result
+}
