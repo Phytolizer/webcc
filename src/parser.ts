9a762a858ea009e354fda1914d0bfb448e4b519c
Kyle Coffey <me@phytolizer.dev>
2023-08-02 18:42:33 -0500

    seriously dumb mistake
    fix #1

--- src/parser.ts
+++ src/parser.ts
@@ -169,31 +169,8 @@ function parseBinaryExpression (
   return left
 }
 
-function parseAssignmentExpression (tokens: Token[]): ast.Expression {
-  if (
-    checkToken(tokens, 'ident', 0) &&
-    assignmentOperators.includes(peek(tokens, 1)?.type)
-  ) {
-    const name = matchToken(tokens, 'ident').value
-    const assignOp = tokens.shift()?.type
-    if (assignOp === undefined) {
-      throw new Error('unreachable')
-    }
-    let expression = parseExpression(tokens)
-    if (assignOp !== '=') {
-      expression = new ast.BinaryOp(
-        new ast.VarExpression(name),
-        assignOp.slice(0, -1),
-        expression
-      )
-    }
-    return new ast.AssignExpression(name, expression)
-  }
-  return parseBinaryExpression(tokens)
-}
-
 function parseExpression (tokens: Token[]): ast.Expression {
-  return parseAssignmentExpression(tokens)
+  return parseBinaryExpression(tokens)
 }
 
 function parseStatement (tokens: Token[]): ast.Statement {
